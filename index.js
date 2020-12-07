c67ce1cc8d71b8e9d82d88bfac098a4b7a1710cd
Rika <rika4886@gmail.com>
2020-12-07 18:02:34 +0100

    updated

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ function showMenu () {
   const menuDivs = document.querySelectorAll('.menu-container');
 
   menuDivs.forEach((div) => {
-
+    // div.style.display = "block"
     if(div.style.display === "none"){
       div.style.display = "block";
     } else{
       div.style.display = "none";
     }
+
   })
 }
 
@@ -24,8 +25,34 @@ function drinkMenu () {
     } else{
       div.style.display = "none";
     }
+
   })
 }
 
 menuBtn.addEventListener('click', showMenu);
 drinkBtn.addEventListener('click', drinkMenu);
+
+$(document).ready(function() {
+  $("#myCarousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
