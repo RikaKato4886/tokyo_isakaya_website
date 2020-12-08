1d41a4b1f540771d96e5a7ad98b18e6e61e44016
Rika <rika4886@gmail.com>
2020-12-08 20:14:13 +0100

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menuButton');
 const drinkBtn = document.querySelector('.drink-Button');
 
-function showMenu () {
+const showMenu = () => {
   const menuDivs = document.querySelectorAll('.menu-container');
 
   menuDivs.forEach((div) => {
@@ -15,7 +15,7 @@ function showMenu () {
   })
 }
 
-function drinkMenu () {
+const drinkMenu = () =>  {
   const drinkDivs = document.querySelectorAll('.drink-menu-container');
 
   drinkDivs.forEach((div) => {
@@ -31,28 +31,3 @@ function drinkMenu () {
 
 menuBtn.addEventListener('click', showMenu);
 drinkBtn.addEventListener('click', drinkMenu);
-
-$(document).ready(function() {
-  $("#myCarousel").on("slide.bs.carousel", function(e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 3;
-    var totalItems = $(".carousel-item").length;
-
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i = 0; i < it; i++) {
-        // append slides to end
-        if (e.direction == "left") {
-          $(".carousel-item")
-            .eq(i)
-            .appendTo(".carousel-inner");
-        } else {
-          $(".carousel-item")
-            .eq(0)
-            .appendTo($(this).find(".carousel-inner"));
-        }
-      }
-    }
-  });
-});
