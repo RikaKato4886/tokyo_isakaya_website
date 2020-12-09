a62e81d74507cc9313e8532ccf5a89d487658589
Rika <rika4886@gmail.com>
2020-12-09 21:53:03 +0100

    updated

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const menuBtn = document.querySelector('.menuButton');
-const drinkBtn = document.querySelector('.drink-Button');
+// const menuBtn = document.querySelector('.menuButton');
+// const drinkBtn = document.querySelector('.drink-Button');
 
-const showMenu = () => {
-  const menuDivs = document.querySelectorAll('.menu-container');
+// const showMenu = () => {
+//   const menuDivs = document.querySelectorAll('.menu-container');
 
-  menuDivs.forEach((div) => {
-    // div.style.display = "block"
-    if(div.style.display === "none"){
-      div.style.display = "block";
-    } else{
-      div.style.display = "none";
-    }
+//   menuDivs.forEach((div) => {
+//     // div.style.display = "block"
+//     if(div.style.display === "none"){
+//       div.style.display = "block";
+//     } else{
+//       div.style.display = "none";
+//     }
 
-  })
-}
+//   })
+// }
 
-const drinkMenu = () =>  {
-  const drinkDivs = document.querySelectorAll('.drink-menu-container');
+// const drinkMenu = () =>  {
+//   const drinkDivs = document.querySelectorAll('.drink-menu-container');
 
-  drinkDivs.forEach((div) => {
+//   drinkDivs.forEach((div) => {
 
-    if(div.style.display === "none"){
-      div.style.display = "block";
-    } else{
-      div.style.display = "none";
-    }
+//     if(div.style.display === "none"){
+//       div.style.display = "block";
+//     } else{
+//       div.style.display = "none";
+//     }
 
-  })
-}
+//   })
+// }
 
-menuBtn.addEventListener('click', showMenu);
-drinkBtn.addEventListener('click', drinkMenu);
+// menuBtn.addEventListener('click', showMenu);
+// drinkBtn.addEventListener('click', drinkMenu);
